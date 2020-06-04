418712b8baf44cc22b6945db7f7782a5584c5cc5
yencis <47923945+yencis@users.noreply.github.com>
2020-06-03 20:55:07 -0700

    sasasasas

--- scripts.js
+++ scripts.js
@@ -1,6 +1,6 @@
 function update_button(){
     var nButton = document.getElementsByClassName("nav-button")[0];
-    if (window.scrollY>221){
+    if (window.scrollY>300){
         nButton.style.color = "#FFFFFF";
     }else{
         nButton.style.color = "#3264b0";
