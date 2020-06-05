0a7466452e9411cd407e8f85c1c21a35a35ff070
yencis <47923945+yencis@users.noreply.github.com>
2020-06-04 18:26:29 -0700

    404 page

--- scripts.js
+++ scripts.js
@@ -1,3 +1,4 @@
+
 function update_button(){
     var nButton = document.getElementsByClassName("nav-button")[0];
     var navBar = document.getElementsByClassName("navbar-new")[0];
@@ -6,4 +7,26 @@ function update_button(){
     }else{
         nButton.style.color = "#3264b0";
     }
+}
+
+function flicker(){
+    var i = parseInt(Math.random()*4);
+    document.getElementsByClassName("text")[i].style.color = "#474747";
+    document.getElementsByClassName("text")[i].style.animation= "none";
+    console.log("hello");
+
+    setTimeout(function() {
+        console.log("helo2");
+        document.getElementsByClassName("text")[i].style.color = "white";
+        document.getElementsByClassName("text")[i].style.animation = "glow 1s ease-in-out infinite alternate";
+    }
+    ,1000);
+}
+
+function stopFlicker(){
+    flickerStatus = 0;
+}
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
 }
