4d47f885ef5da93cf28041f792c7dae4845c59ca
yencis <47923945+yencis@users.noreply.github.com>
2021-01-05 23:46:37 -0800

    Sheeesh

--- chart.js
+++ chart.js
@@ -96,6 +96,7 @@ xAxis.attr("transform", `translate(${0},${height})`);
     .attr('width', yScale.bandwidth());*/
 
 const format = xScale.tickFormat(20, sample.format)
+
 chart.append("g")
     .attr("fill", "#C3DCFF")
     .selectAll("rect")
@@ -103,11 +104,12 @@ chart.append("g")
     .join("rect")
     //.attr('font-family','FontAwesome')
     //.attr("xlink:href", "https://github.com/favicon.ico")
+   // .attr("z-index",-2)
+    .attr("id","bg")
     .attr("x", xScale(0))
     .attr("y", (d, i) => yScale(d.language))
     .attr("width", d => width)
     .attr("height", yScale.bandwidth());
-
 
 chart.append("g")
     .attr("fill", "#2469CD")
@@ -116,11 +118,68 @@ chart.append("g")
     .join("rect")
     //.attr('font-family','FontAwesome')
     //.attr("xlink:href", "https://github.com/favicon.ico")
+   // .attr("z-index",5)
+    .attr("id","rects")
     .attr("x", xScale(0))
     .attr("y", (d, i) => yScale(d.language))
-    .attr("width", d => xScale(d.value) - xScale(0))
+    //.attr("width", d => width- xScale(0))
+    .attr("width",d=>xScale(0))
+    //.attr("width", d => height-yScale(0))
     .attr("height", yScale.bandwidth());
 
+/*
+    chart.selectAll("#rects")
+        .transition()
+        .duration(800)
+        .attr("width", d => xScale(d.value) - xScale(0))
+        .delay(function(d,i){console.log(i) ; return(i*100)});
+*/
+$(document).ready(function () {
+    function isScrolledIntoView(elem) {
+        var docViewTop = $(window).scrollTop();
+        var docViewBottom = docViewTop + $(window).height();
 
+        var elemTop = $(elem).offset().top;
+        var elemBottom = elemTop + $(elem).height();
 
+        var elemID = $(elem).attr("id");
+        var elemTag = $(elem).prop("tagName");
 
+        return elemTop <= docViewBottom &&
+        elemBottom >= docViewTop &&
+        elemTag == "svg"
+            ? elemID
+            : false;
+    }
+
+    $(window).scroll(function () {
+        //console.log("e");
+        $("svg").each(function () {
+            var ele = isScrolledIntoView(this);
+           // console.log(ele);
+            if (ele) {
+                var ele_ = ele.replace(/-/g, "_");
+
+                var fn = eval(ele_);
+                var checkEle = $("#" + ele).attr("data-enable");
+
+                if (!checkEle) {
+                    //   console.log(ele);
+                    $(this).attr("data-enable", "true");
+                    setTimeout(() => {
+                        console.log("e");
+                        chart.selectAll("#rects")
+                            .transition()
+                            .duration(600)
+                            .attr("width", d => xScale(d.value) - xScale(0))
+                            .delay(function (d, i) {
+                                console.log(i);
+                                return (i * 100)
+                            });
+                    }, 100);
+                }
+            }
+        });
+    });
+});
+    // function that starts drawing d3 chart
--- classscripts.js
+++ classscripts.js
@@ -1,8 +1,35 @@
 $(document).ready(function(){
 
 
+    var inView = false;
+
+    function isScrolledIntoView(elem)
+    {
+        var docViewTop = $(window).scrollTop();
+        var docViewBottom = docViewTop + $(window).height();
+
+        var elemTop = $(elem).offset().top;
+        var elemBottom = elemTop + $(elem).height();
+
+        return ((elemTop <= docViewBottom) && (elemBottom >= docViewTop));
+    }
+
+
 
     $(window).scroll(function(){
+
+/*
+        $(".load-on-scroll").each(function(i){
+            if (isScrolledIntoView(this)) {
+                if (inView) { return; }
+                inView = true;
+                $(this).style.opacity
+            } else {
+                inView = false;
+            }
+
+        })
+*/
 
         $(".fade-in-onscroll").each(function(i){
             var objbot= $(this).position().top + $(this).outerHeight();
@@ -15,3 +42,5 @@ $(document).ready(function(){
         })
     })
 })
+
+
