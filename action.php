0784cd28e2ebbb45bf4140689c44e4f47367bf94
yencis <47923945+yencis@users.noreply.github.com>
2021-01-08 17:21:22 -0800

    Yikes :grimacing:

--- action.php
+++ action.php
@@ -1,10 +1,47 @@
-<html>
-<body>
+
+<?php
+echo "peopeod";
+use PHPMailer\PHPMailer\PHPMailer;
+
+$name = $email = $message = "";
+
+if (isset($_POST['submit'] {
+  $name = test_input($_POST["name"]);
+  $email = test_input($_POST["email"]);
+  $message = test_input($_POST["message"]);
+
+  require_once "PHPMailer/PHPMailer.php"
+  require_once "PHPMailer/SMTP.php"
+  require_once "PHPMailer/Exception.php"
+
+  $mail = new PHPMailer();
+
+  $mail->isSMTP();
+  $mail->Host = "smtp.gmail.com";
+  $mail->SMTPAuth = true;
+  $mail->Username = "apithanos2004@gmail.com";
+  $mail->Password = "no_password_for_you";
+  $mail->Port = 465;
+  $mail->SMTPSecure = "ssl";
+
+  $mail->isHTML(true);
+  $mail->setFrom($email,$name);
+  $mail->addAddress("apithanos2004@gmail.com");
+  $mail->Subject = ("$email ");
+  $mail->Body = $message;
+
+  if ($mail->send()){
+    $status = "success";
+    $response = "Email is sent";
+  }else{
+    $status = "failed";
+    $response = "Something is wrong: <br>" . $mail->ErrorInfo;
+  }
+    echo $status;
+    echo $response
+  exit(json_encode(array("status"=>$status,"response"=> $response)));
 
 
+}
 
-Welcome <?php echo $_POST["name"]; ?><br>
-Your email address is: <?php echo $_POST["email"]; ?>
-
-</body>
-</html>
+?>
